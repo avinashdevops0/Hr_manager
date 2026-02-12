11620e4fdd306d490715b2b417eaccaec8df6d0b
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-02-12 18:49:53 +0530

    hr

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -18,10 +18,15 @@ app.use(morgan('combined'));
 app.use(express.static('public'));
 
 // Service routes
-app.use('/api/employees', proxy('http://localhost:3001'));
-app.use('/api/attendance', proxy('http://localhost:3002'));
-app.use('/api/leaves', proxy('http://localhost:3003'));
-app.use('/api/payroll', proxy('http://localhost:3004'));
+// app.use('/api/employees', proxy('http://localhost:3001'));
+// app.use('/api/attendance', proxy('http://localhost:3002'));
+// app.use('/api/leaves', proxy('http://localhost:3003'));
+// app.use('/api/payroll', proxy('http://localhost:3004'));
+
+app.use('/api/employees', proxy('http://employee-service:3001'));
+app.use('/api/attendance', proxy('http://attendence-service:3002'));
+app.use('/api/leaves', proxy('http://leave-service:3003'));
+app.use('/api/payroll', proxy('http://payroll-service:3004'));
 
 // Health check
 app.get('/health', (req, res) => {
@@ -29,10 +34,15 @@ app.get('/health', (req, res) => {
         status: 'UP', 
         timestamp: new Date(),
         services: {
-            employees: 'http://localhost:3001',
-            attendance: 'http://localhost:3002',
-            leaves: 'http://localhost:3003',
-            payroll: 'http://localhost:3004'
+            // employees: 'http://localhost:3001',
+            // attendance: 'http://localhost:3002',
+            // leaves: 'http://localhost:3003',
+            // payroll: 'http://localhost:3004'
+
+            employees: 'http://employee-service:3001',
+            attendance: 'http://attendence-service:3002',
+            leaves: 'http://leave-service:3003',
+            payroll: 'http://payroll-service:3004'
         }
     });
 });
